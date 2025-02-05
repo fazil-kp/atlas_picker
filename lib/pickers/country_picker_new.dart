f12b73e163bdfb63dc041ba313ee246f560be259
Fazil <104255104+fazil-kp@users.noreply.github.com>
2025-02-05 23:01:14 +0530

    update country_list.dart, country_picker_new.dart and state_picker_new.dart

--- lib/pickers/country_picker_new.dart
+++ lib/pickers/country_picker_new.dart
@@ -15,20 +15,11 @@ class CountryPickerNew extends HookWidget {
   final bool? isSearchEnable;
   final Color? searchBoarderColor;
   final PickerType pickerType;
-  const CountryPickerNew(
-      {super.key,
-      required this.pickerType,
-      this.searchBoarderColor,
-      required this.onChanged,
-      this.customWidget,
-      this.verticalPadding,
-      this.horizontalPadding,
-      this.isSearchEnable = true});
+  const CountryPickerNew({super.key, required this.pickerType, this.searchBoarderColor, required this.onChanged, this.customWidget, this.verticalPadding, this.horizontalPadding, this.isSearchEnable = true});
 
   @override
   Widget build(BuildContext context) {
-    final filteredCountries =
-        useState<List<CountryModel>>(ListOfCountries.listOfCountries);
+    final filteredCountries = useState<List<CountryModel>>(CountryList.countryList);
     final selectedCountry = useState<CountryModel?>(null);
     final searchText = useState("");
     final searchFocus = useFocusNode();
@@ -36,13 +27,9 @@ class CountryPickerNew extends HookWidget {
     void searchCountries(String value) {
       searchText.value = value;
       if (value.isEmpty) {
-        filteredCountries.value = ListOfCountries.listOfCountries;
+        filteredCountries.value = CountryList.countryList;
       } else {
-        filteredCountries.value = ListOfCountries.listOfCountries
-            .where((country) =>
-                (country.name).toLowerCase().contains(value.toLowerCase()) ||
-                (country.code).toLowerCase().contains(value.toLowerCase()))
-            .toList();
+        filteredCountries.value = CountryList.countryList.where((country) => (country.name).toLowerCase().contains(value.toLowerCase()) || (country.code).toLowerCase().contains(value.toLowerCase())).toList();
       }
     }
 
@@ -58,21 +45,13 @@ class CountryPickerNew extends HookWidget {
                 height: 45,
                 width: 210,
                 padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(color: Colors.grey.shade300)),
+                decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(10), border: Border.all(color: Colors.grey.shade300)),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Text(selectedCountry.value?.name ?? 'Select a country',
-                        style: const TextStyle(
-                            fontSize: 13,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black)),
+                    Text(selectedCountry.value?.name ?? 'Select a country', style: const TextStyle(fontSize: 13, fontWeight: FontWeight.w500, color: Colors.black)),
                     const Spacer(),
-                    const Icon(Icons.keyboard_arrow_down_outlined,
-                        size: 24, color: Colors.black),
+                    const Icon(Icons.keyboard_arrow_down_outlined, size: 24, color: Colors.black),
                   ],
                 ),
               ),
@@ -85,13 +64,7 @@ class CountryPickerNew extends HookWidget {
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.black.withOpacity(0.1),
-                blurRadius: 5,
-                spreadRadius: 0,
-                offset: const Offset(0, 2))
-          ],
+          boxShadow: [BoxShadow(color: Colors.black.withOpacity(0.1), blurRadius: 5, spreadRadius: 0, offset: const Offset(0, 2))],
           border: Border.all(color: Colors.grey.shade200),
         ),
         child: Column(
@@ -112,27 +85,12 @@ class CountryPickerNew extends HookWidget {
                       focusColor: Colors.white,
                       fillColor: Colors.white,
                       hintText: 'Search Country',
-                      hintStyle:
-                          const TextStyle(color: Colors.grey, fontSize: 13),
-                      prefixIcon: PickerTypeSearch()
-                                  .buildPickerTypeSearch(pickerType) ==
-                              true
-                          ? const Icon(Icons.search,
-                              color: Colors.grey, size: 20)
-                          : null,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(color: Colors.grey.shade200)),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(
-                              color:
-                                  searchBoarderColor ?? Colors.red.shade300)),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(color: Colors.grey.shade300)),
-                      contentPadding: const EdgeInsets.symmetric(
-                          vertical: 5, horizontal: 16),
+                      hintStyle: const TextStyle(color: Colors.grey, fontSize: 13),
+                      prefixIcon: PickerTypeSearch().buildPickerTypeSearch(pickerType) == true ? const Icon(Icons.search, color: Colors.grey, size: 20) : null,
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: BorderSide(color: Colors.grey.shade200)),
+                      focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: BorderSide(color: searchBoarderColor ?? Colors.red.shade300)),
+                      enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: BorderSide(color: Colors.grey.shade300)),
+                      contentPadding: const EdgeInsets.symmetric(vertical: 5, horizontal: 16),
                     ),
                   ),
                 ),
@@ -149,15 +107,8 @@ class CountryPickerNew extends HookWidget {
                       onTap: () {
                         if (selectedCountry.value == country) {
                           selectedCountry.value = null;
-                          onChanged.call(const CountryModel(
-                              name: '',
-                              code: '',
-                              flag: '',
-                              shortName: '',
-                              phoneNumberLength: 0,
-                              states: []));
-                          filteredCountries.value =
-                              ListOfCountries.listOfCountries;
+                          onChanged.call(const CountryModel(name: '', code: '', flag: '', shortName: '', phoneNumberLength: 0, states: []));
+                          filteredCountries.value = CountryList.countryList;
                         } else {
                           selectedCountry.value = country;
                           onChanged.call(country);
@@ -165,18 +116,11 @@ class CountryPickerNew extends HookWidget {
                         close.call();
                       },
                       child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 10, vertical: 5),
+                        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
                         child: Container(
-                          decoration: BoxDecoration(
-                              color: isSelected
-                                  ? Colors.grey.shade300
-                                  : Colors.white,
-                              borderRadius: BorderRadius.circular(10)),
-                          padding: const EdgeInsets.only(
-                              top: 10, bottom: 10, left: 8),
-                          child: PickerTypeUi()
-                              .buildPickerTypeWidget(pickerType, country),
+                          decoration: BoxDecoration(color: isSelected ? Colors.grey.shade300 : Colors.white, borderRadius: BorderRadius.circular(10)),
+                          padding: const EdgeInsets.only(top: 10, bottom: 10, left: 8),
+                          child: PickerTypeUi().buildPickerTypeWidget(pickerType, country),
                         ),
                       ),
                     ),
@@ -191,15 +135,4 @@ class CountryPickerNew extends HookWidget {
   }
 }
 
-enum PickerType {
-  normal,
-  all,
-  flagOnly,
-  codeOnly,
-  nameOnly,
-  flagAndName,
-  shortNameOnly,
-  flagAndShortName,
-  flagAndCode,
-  codeAndName
-}
+enum PickerType { normal, all, flagOnly, codeOnly, nameOnly, flagAndName, shortNameOnly, flagAndShortName, flagAndCode, codeAndName }
